20164059953454c77e55336615f0a33cd42eeb80
sohan1 <shn619@gmail.com>
2016-02-16 12:44:01 +0530

    chnages for menu

--- js/templateservice.js
+++ js/templateservice.js
@@ -8,12 +8,10 @@ templateservicemod.service('TemplateService', function() {
     this.year = d.getFullYear();
 
     this.init = function() {
-        this.headermenu = "views/headermenu.html";
+        this.sidemenu = "views/sidemenu.html";
         this.header = "views/header.html";
         this.menu = "views/menu.html";
-        this.slider = "views/slider.html";
         this.content = "views/content/content.html";
-        this.footermenu = "views/footermenu.html";
         this.footer = "views/footer.html";
     };
 
@@ -23,7 +21,7 @@ templateservicemod.service('TemplateService', function() {
         data.content = "views/content/" + page + ".html";
         return data;
     };
-    
+
     this.init();
 
 });
